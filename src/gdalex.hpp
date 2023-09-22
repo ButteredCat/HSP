437b115d9ed136a33d5a0fcbee9be27c76c10ca9
xiaoyc <xiao.yc@foxmail.com>
2023-09-22 17:42:51 +0800

    新增注释

--- src/gdalex.hpp
+++ src/gdalex.hpp
@@ -1,4 +1,13 @@
-// Copyright (C) 2023 Xiao Yunchen
+/**
+ * @file gdalex.hpp
+ * @author xiaoyc
+ * @brief GDAL扩展
+ * @version 0.1
+ * @date 2023-09-22
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 #ifndef SRC_GDALEX_HPP_
 #define SRC_GDALEX_HPP_
 
@@ -26,6 +35,13 @@ inline char* strlwr(char* str) {
   return orig;
 }
 
+/**
+ * @brief
+ *
+ * @param ext
+ * @param default_ret
+ * @return const char*
+ */
 inline const char* GetGDALDescription(const char* ext,
                                       const char* default_ret) {
   static const std::map<std::string, std::string> dictionary = {
@@ -42,6 +58,13 @@ inline const char* GetGDALDescription(const char* ext,
   return dictionary.at(extension).c_str();
 }
 
+/**
+ * @brief 判断文件filepath是否是栅格数据
+ *
+ * @param filepath 文件完整路径
+ * @return true
+ * @return false
+ */
 inline bool IsRasterDataset(const char* filepath) {
   FILE* fp = fopen(filepath, "r");
   if (fp == nullptr) {
@@ -66,6 +89,12 @@ inline bool IsRasterDataset(const char* filepath) {
   return true;
 }
 
+/**
+ * @brief 获取GDAL定义的数据类型所能表示的值的上下限
+ *
+ * @param type GDAL定义的数据类型
+ * @return std::pair<double, double>
+ */
 inline std::pair<double, double> GetDataTypeMinMax(GDALDataType type) {
   switch (type) {
     case GDT_Byte:
@@ -83,6 +112,17 @@ inline std::pair<double, double> GetDataTypeMinMax(GDALDataType type) {
   }
 }
 
+/**
+ * @brief
+ * 根据待创建文件名的后缀，创建相应的GDALDriver，并以此创建数据集（GDALDataset）
+ *
+ * @param filepath 待创建文件路径
+ * @param cols 待创建数据集的列数（即 samples）
+ * @param rows 待创建数据集的行数（即 lines）
+ * @param bands 待创建数据集的波段数
+ * @param type 待创建数据集的像元数据类型
+ * @return GDALDataset*
+ */
 inline GDALDataset* GDALCreate(const char* filepath, int cols, int rows,
                                int bands, GDALDataType type) {
   GDALDataset* dataset;
